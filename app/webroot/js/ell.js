f304b8fc288647b564e3d9ef75abb578019cb741
mzmttks <ta.mizumoto@gmail.com>
2014-12-02 20:33:40 +0900

    add linechart

--- app/webroot/js/ell.js
+++ app/webroot/js/ell.js
@@ -79,24 +79,65 @@ function createPiSelfData(jsonData) {
     return data;
 }
 
+
 function drawLineSelf() {
-    var data = google.visualization.arrayToDataTable([
-        ['Time', 'Happy', 'Sad'],
-        ['2014-11-08 17:00:00', 1000, 400],
-        ['2014-11-08 18:00:00', 1170, 460],
-        ['2014-11-09 19:00:00', 660, 1120],
-        ['2014-11-09 20:00:00', 1030, 540]
-    ]);
+    // var jsonData = 
+    // [ {"Emotion": {"record_date":"2014-12-02 07:31:23",
+    //                "my_emotion":"fear","my_emotion_val":"50"}},
+    //   {"Emotion": {"record_date":"2014-12-02 07:31:31",
+    //                "my_emotion":"angry","my_emotion_val":"66"}},
+    //   {"Emotion": {"record_date":"2014-12-02 07:31:43",
+    //                "my_emotion":"sad","my_emotion_val":"27"}},
+    //   {"Emotion": {"record_date":"2014-12-02 07:31:53",
+    //                "my_emotion":"happy","my_emotion_val":"85"}},
+    //   {"Emotion": {"record_date":"2014-12-02 07:32:04",
+    //                "my_emotion":"happy","my_emotion_val":"58"}}]
+    $.getJSON("/EmotionLifeLog/emotions/getHistorical", {},
+        function(jsonData) {
+            var data = createLineSelfData(jsonData);
+            console.log(data);
+            var chart = new google.visualization.LineChart(document.getElementById('line-self'));
+            var options = {
+                title: "自己評価の時系列 (割合)",
+                colors: ['red', 'green', 'orange', 'blue'];
+            }
+
+            chart.draw(data, options);
+    });
 
     var options = {
         title: '自己評価の感情（時系列）'
     };
-
-    var chart = new google.visualization.LineChart(document.getElementById('line-self'));
-
-    chart.draw(data, options);
 }
 
+
+function createLineSelfData(jsonData) {
+    var data = new google.visualization.DataTable();
+    data.addRows(jsonData.length);
+    data.addColumn("string", "Date");
+    data.addColumn("number", "happy");
+    data.addColumn("number", "angry");
+    data.addColumn("number", "sad");
+    data.addColumn("number", "fear");
+    $.each(jsonData, function(i, v) {
+        data.setValue(i, 0, v.Emotion.record_date);
+        switch(v.Emotion.my_emotion) {
+          case "happy":
+              data.setValue(i, 1, v.Emotion.my_emotion_val);
+              break;
+          case "angry":
+              data.setValue(i, 2, v.Emotion.my_emotion_val);
+              break;
+          case "sad":
+              data.setValue(i, 3, v.Emotion.my_emotion_val);
+              break;
+          case "fear":
+              data.setValue(i, 4, v.Emotion.my_emotion_val);
+              break;
+        }
+    });
+    return data;
+}
 function showValue() {
     document.getElementById("show-range").innerHTML = document.getElementById("range").value;
 }
